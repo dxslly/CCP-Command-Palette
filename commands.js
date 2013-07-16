bbc4e39e9749291be49e5da61a8cb4959029ad05
unknown <davidHelmon@gmail.com>
2013-07-16 17:41:29 -0400

    First Commit

---
+++ commands.js
@@ -0,0 +1,25 @@
+commands = [
+	newTab
+];
+
+/*
+Command Object:
+	slug - String: A short string representation with no spaces
+	name - String: A full string of the command's name
+	description - String: A brief description of what the command does
+	functionCall - Function: The function that is called when the command is run
+*/
+
+var newTab = {
+	slug: 'newtab',
+	name: 'New Tab',
+	description: 'Opens and switches to a new tab',
+	functionCall: 'newTab'
+}
+
+function newTab(url) {
+	var createProperties = {}
+	if (url)
+		createProperties.url = url;
+	chrome.tabs.create(createProperties);
+}
---
+++ popup.js
@@ -0,0 +1,29 @@
+/*
+	Calls a function by a string and passes the params
+*/
+function callFunction(functionName, params) {
+
+}
+
+
+var suggestionID = 'commandSuggestion';
+
+function populateSuggestions() {
+	var suggestions = $('#suggestions');
+
+	suggestions.empty();
+
+	function createSuggestion(command) {
+		var suggestion = $('<a>');
+		suggestion.addClass('suggestion');
+		suggestion.data('command', command.slug);
+		suggestion.append($('<span>').addClass('command').text(command.name));
+		suggestion.append($('<span>').addClass('description').text(command.description));
+		return suggestion;
+	}
+
+	for (var i = commands.length - 1; i >= 0; i--) {
+		var suggestion = createSuggestion(commands[i]);
+		suggestions.append(suggestion);
+	};
+}
