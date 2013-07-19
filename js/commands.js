c663b0be771f628208f3ae4574c6577038ca0202
David Samuelson <davidhlemon@gmail.com>
2013-07-19 01:21:28 -0400

    Added several new commands, including switching tabs :), and several Goto commands.  Scrolling through the suggestions is bugged.

--- js/commands.js
+++ js/commands.js
@@ -12,32 +12,70 @@
 
 _suggestions = [
 	{ 'caption': 'About', 'command': 'openNewTab', 'args': {'url': 'about.html'} },
-	{ 'caption': 'Browser Data: Delecte Cache', 'command': 'deleteCache', 'shortcut': {'windows': ['Ctrl','Shift','Delete']} },
-	{ 'caption': 'Debug: Open WebCommand in Tab', 'command': 'openNewTab', 'args': {'url': 'popup.hmtl'} },
+	{ 'caption': 'Browser Data: Delete Cache', 'command': 'deleteCache', 'shortcut': {'windows': ['Ctrl','Shift','Delete']} },
+	{ 'caption': "Debug: Echo Value", 'command': 'askForEchoValue'},
+	{ 'caption': 'Debug: Open WebCommand in Tab', 'command': 'openNewTab', 'args': {'url': 'popup.html'} },
+	{ 'caption': 'Goto: Downloads', 'command': 'openNewTab', 'args': {'url': 'chrome://downloads/'}, 'shortcut': {'windows': ['Ctrl','Shift','J'], 'mac': ['⌘','Shift','J']} },
+	{ 'caption': 'Goto: Extentions', 'command': 'openNewTab', 'args': {'url': 'chrome://extensions/'} },
+	{ 'caption': 'Goto: History', 'command': 'openNewTab', 'args': {'url': 'chrome://history/'}, 'shortcut': {'windows': ['Ctrl','H'], 'mac': ['⌘','H']}},
+	{ 'caption': 'Goto: Settings', 'command': 'openNewTab', 'args': {'url': 'chrome://settings/'} },
+	{ 'caption': 'Page: Print', 'command': 'printPage', 'shortcut': {'windows': ['Ctrl','P'], 'mac': ['⌘','P']} },
+	{ 'caption': 'Tab: Switch To', 'command': 'suggestTabs' },
 	{ 'caption': 'Tab: Close Current', 'command': 'closeCurrentTab', 'shortcut': {'windows': ['Ctrl','W'], 'mac': ['⌘','W']} },
 	{ 'caption': 'Tab: Duplicate Current', 'command': 'duplicateCurrentTab' },
 	{ 'caption': 'Tab: Open New', 'command': 'openNewTab', 'shortcut': {'windows': ['Ctrl','T'], 'mac': ['⌘','T']} },
 	{ 'caption': 'Tab: Reload Current',	'command': 'reloadCurrentTab', 'shortcut': {'windows': ['Ctrl','R'], 'mac': ['⌘','R']} },
-	{ 'caption': 'Page: Print', 'command': 'printPage', 'shortcut': {'windows': ['Ctrl','P'], 'mac': ['⌘','P']} }
+	{ 'caption': 'Window: Open New', 'command': 'createWindow', 'shortcut': {'windows': ['Ctrl','N'], 'mac': ['⌘','N']} }
 ];
 
-// Functions
+// Suggest Functions. Pass object to suggestion's callBacks
+
+function suggestTabs() {
+	chrome.windows.getAll({ 'populate': true }, function(windows) {
+		var suggestions = [];
+		for (var i = windows.length - 1; i >= 0; i--) {
+			for (var j = windows[i].tabs.length - 1; j >= 0; j--) {
+				var suggestion = {};
+				var currentTab = windows[i].tabs[j];
+				suggestion.caption = currentTab.url;
+				suggestion.command = 'updateTab';
+				suggestion.args = { 'tabId': currentTab.id, 'updateProperties': { 'active': true } };
+				suggestion.image = currentTab.favIconUrl;
+				suggestions.push(suggestion);
+			};
+		};
+		suggestToUser(suggestions, 'Search or Type a Tab');
+	});
+}
+
+// Ask Functions. Pass string to callBack
+
+function askForEchoValue() {
+	askUser('Type a value to echo', function(str) {
+		alert(str);
+	});
+}
+
+// Normies
+
+function updateTab(obj) {
+	chrome.tabs.update(obj.tabId, obj.updateProperties);
+}
 
 function printPage() {
 	chrome.tabs.update(null, {url: 'javascript:window.print();'});
 }
 
 function log(obj) {
-	console.log('Test funciton called');
+	console.log(obj);
 }
 
 function closeCurrentTab() {
 	chrome.windows.getCurrent({populate: true}, function(window) {
 		console.log(window);
 		for (var i = window.tabs.length - 1; i >= 0; i--) {
-			if (window.tabs[i].active) {
+			if (window.tabs[i].active)
 				chrome.tabs.remove(window.tabs[i].id);
-			}
 		};
 	});
 }
@@ -52,10 +90,13 @@ function reloadCurrentTab() {
 	chrome.tabs.reload();
 }
 
+function createWindow(obj) {
+	chrome.window.create(obj);
+}
+
 function deleteCache() {
 	chrome.browsingData.removeCache({});
 }
-
 
 function openNewTab(obj) {
 	chrome.tabs.create(obj);
--- js/popup.js
+++ js/popup.js
@@ -9,14 +9,17 @@
 	'linux': 3
 }
 
+var DEFAULT_PLACEHOLDER = 'Search or Type a Command';
+
 /* ==============================================================================
  *     VARIABLES
  * ==============================================================================
  */
-var _currentfuzzySearch;
+var _currentFuzzySearch;
 var _currentSuggestions;
 var _currentCallBack;
 var _os = detectOS();
+
 
 /* ==============================================================================
  *     FUNCTIONS
@@ -27,102 +30,135 @@ var _os = detectOS();
  * Repopulates the suggestions with 
  * ask(arrayOfSuggestions, callBack(inputvalue:string) - optional)
  */
-function askUser(suggestions, prompt, callBack) {
+function suggestToUser(suggestions, placeholder) {
+	_callBack = null;
+	
+	$('#commandField').val('');
+	if (typeof placeholder === 'undefined')
+		placeholder = DEFAULT_PLACEHOLDER;
+	$('#commandField').attr('placeholder', placeholder);
+
 	var options = {
 		keys: ['caption']
 	}
-	_currentfuzzySearch = new Fuse(suggestions, options);
+	_currentFuzzySearch = new Fuse(suggestions, options);
 	_currentSuggestions = suggestions;
-	// @TODO: add prompt code and html
-	_currentCallBack = callBack || null;
+
 	populateSuggestions();
 }
+
+
+function askUser(placeholder, callBack) {
+	_currentSuggestions = null;
+	_currentFuzzySearch = null;
+
+	$('#commandField').val('');
+	$('#suggestions').empty();
+	if (typeof placeholder === 'undefined')
+		placeholder = DEFAULT_PLACEHOLDER;
+	$('#commandField').attr('placeholder', placeholder);
+	_currentCallBack = callBack;
+}
+
 
 /*
  * Called when the user chooses a suggestion or if not suggestions are given
  * when the user presses enter. 
  */
 function onUserChoice() {
-	var selected = $('.selected');
-	if (selected) {
-		var command = $(selected).data('command');
-		var params = $(selected).data('params');
-		callFunctionFromStr(command, params);
-	}
-	if (_currentCallBack)
+	if (_currentCallBack) {
 		_currentCallBack($('#commandField').val());
+	} else {
+		var selected = $('.selected');
+		var command = $(selected).data('command');
+		var args = $(selected).data('args');
+		callFunctionFromStr(command, args);
+	}
+	// window.close();
 }
 
 /*
  * Clears and then fills the suggestions with possible suggestions
  */
 function populateSuggestions() {
-	var suggestionsElement = $('#suggestions');
+	if (_currentSuggestions) {
+		var suggestionsElement = $('#suggestions');
 
-	suggestionsElement.empty();
+		suggestionsElement.empty();
 
-	/*
-	 * Creates a HTML element given a suggestion
-	 *
-	 * params:
-	 *   suggestionObject - suggestion
-	 * return:
-	 *   element - A suggestion HTML element 
-	 */
-	function createHTMLSuggestion(suggestionObject) {
-		var suggestion = $('<a>');
-		suggestion.addClass('suggestion');
-		$(suggestion).data('command', suggestionObject.command);
-		if (suggestionObject.args)
-			$(suggestion).data('args', suggestionObject.args);
-		if (suggestionObject.image)
-			$(suggestion).append($('<img>').attr(suggestionObject.image).addClass('icon'));
-		suggestion.append($('<span>').addClass('caption').text(suggestionObject.caption));
-		if (suggestionObject.description) 
-			suggestion.append($('<span>').addClass('description').text(suggestionObject.description));
-		if (suggestionObject.shortcut) {
-			var shortcutElement = $('<span>').addClass('shortcut');
-			var shortcutKeys;
-			switch(_os) {
-				case OS.mac:
-					shortcutKeys = suggestionObject.shortcut.windows;
-				case OS.windows:
-					shortcutKeys = suggestionObject.shortcut.windows;
-				case OS.linux:
-					shortcutKeys = suggestionObject.shortcut.windows;
-				default:
-					shortcutKeys = suggestionObject.shortcut.windows;
+		/*
+		 * Creates a HTML element given a suggestion
+		 *
+		 * params:
+		 *   suggestionObject - suggestion
+		 * return:
+		 *   element - A suggestion HTML element 
+		 */
+		function createHTMLSuggestion(suggestionObject) {
+			var suggestion = $('<a>');
+			suggestion.addClass('suggestion');
+			$(suggestion).data('command', suggestionObject.command);
+			if (suggestionObject.args)
+				$(suggestion).data('args', suggestionObject.args);
+			if (suggestionObject.image) {
+				var imgElement = $('<img>');
+				$(imgElement).addClass('icon');
+				$(imgElement).attr('src', suggestionObject.image);
+				$(suggestion).append(imgElement);
 			}
-			for (var i = 0; i < shortcutKeys.length; i++) {
-				$(shortcutElement).append($('<span>').text(shortcutKeys[i]).addClass('key'));
-				if (i != shortcutKeys.length - 1)
-					$(shortcutElement).append('+');
-			};
-			suggestion.append(shortcutElement);
+			suggestion.append($('<span>').addClass('caption').text(suggestionObject.caption));
+			if (suggestionObject.description) 
+				suggestion.append($('<span>').addClass('description').text(suggestionObject.description));
+			if (suggestionObject.shortcut) {
+				var shortcutElement = $('<span>').addClass('shortcut');
+				var shortcutKeys;
+				switch(_os) {
+					case OS.mac:
+						shortcutKeys = suggestionObject.shortcut.mac;
+						break;
+					case OS.windows:
+						shortcutKeys = suggestionObject.shortcut.windows;
+						break;
+					case OS.linux:
+						shortcutKeys = suggestionObject.shortcut.linux; // @TODO: support linux shortcuts
+						break;
+					default:
+						shortcutKeys = suggestionObject.shortcut.windows;
+						break;
+				}
+				if (shortcutKeys) {
+					for (var i = 0; i < shortcutKeys.length; i++) {
+						$(shortcutElement).append($('<span>').text(shortcutKeys[i]).addClass('key'));
+						if (i != shortcutKeys.length - 1)
+							$(shortcutElement).append('+');
+					};
+					suggestion.append(shortcutElement);
+				}
+			}
+			return suggestion;
 		}
-		return suggestion;
-	}
 
-	// The value of the input ignoring bad characters
-	var commandFieldVal = $('#commandField').val().replace(/\W/g, '');
-	// If the input field is empty
-	if (commandFieldVal == '') {
-		// Fill with every suggestion
-		for (var i = _currentSuggestions.length - 1; i >= 0; i--) {
-			var suggestion = createHTMLSuggestion(_currentSuggestions[i]);
-			suggestionsElement.prepend(suggestion);
+		// The value of the input ignoring bad characters
+		var commandFieldVal = $('#commandField').val().replace(/\W/g, '');
+		// If the input field is empty
+		if (commandFieldVal == '') {
+			// Fill with every suggestion
+			for (var i = _currentSuggestions.length - 1; i >= 0; i--) {
+				var suggestion = createHTMLSuggestion(_currentSuggestions[i]);
+				suggestionsElement.prepend(suggestion);
+			}
+			if (_currentSuggestions.length > 0)
+				$('.suggestion').first().addClass('selected');
+		} else {
+			// Fuzzy search for results Fill with results
+			var results = _currentFuzzySearch.search(commandFieldVal);
+			for (var i = results.length - 1; i >= 0; i--) {
+				var suggestion = createHTMLSuggestion(results[i]);
+				suggestionsElement.prepend(suggestion);
+			};
+			if (results.length > 0)
+				$('.suggestion').first().addClass('selected');
 		}
-		if (_currentSuggestions.length > 0)
-			$('.suggestion').first().addClass('selected');
-	} else {
-		// Fuzzy search for results Fill with results
-		var results = _currentfuzzySearch.search(commandFieldVal);
-		for (var i = results.length - 1; i >= 0; i--) {
-			var suggestion = createHTMLSuggestion(results[i]);
-			suggestionsElement.prepend(suggestion);
-		};
-		if (results.length > 0)
-			$('.suggestion').first().addClass('selected');
 	}
 }
 
@@ -135,10 +171,11 @@ function selectSuggestion(suggestionElement) {
 	var selected =  $('.selected');
 	$(selected).removeClass('selected');
 	$(suggestionElement).addClass('selected');
-	$('body').stop(true);
-	$('body').animate({
-		scrollTop: $(suggestionElement).offset().top - (140) // EVIL MAGIC!!
-	}, 100); 
+	$('#container').stop(true);
+	// EVIL MAGIC!!
+	$('#container').animate({
+		scrollTop: $(suggestionElement).offset().top - 140
+	}, 100);
 }
 
 
@@ -150,7 +187,7 @@ $(document).ready(function(){
 	// Bring focus to the input box, to make the user feel safe
 	$('#commandField').focus();
 	// Ask user about command suggestions 
-	askUser(getCommandSuggestions());
+	suggestToUser(getCommandSuggestions());
 	
 	$('#commandField').on('input', function() {
 		populateSuggestions();
@@ -160,28 +197,28 @@ $(document).ready(function(){
 	$(document).on('keydown', function(e) {
 		$('#commandField').focus();
 		var selected =  $('.selected');
-		if (e.which == 40 || e.which == 9) { // if down arrow or tab
-			e.preventDefault();
-			var next = (selected).next('.suggestion');
-			if (next.length != 0) {
-				selectSuggestion(next);
-			} else {
-				var first = $('.suggestion').first();
-				if (first)
-					selectSuggestion(first);
+		if (selected) {
+			if (e.which == 40 || e.which == 9) { // if down arrow or tab
+				e.preventDefault();
+				var next = (selected).next('.suggestion');
+				if (next.length != 0) {
+					selectSuggestion(next);
+				} else {
+					var first = $('.suggestion').first();
+					if (first)
+						selectSuggestion(first);
+				}
+			} else if (e.which == 38) { // if up arrow
+				e.preventDefault();
+				var prev = (selected).prev('.suggestion');
+				if (prev.length != 0) {
+					selectSuggestion(prev);
+				} else {
+					var last = $('.suggestion').last();
+					if (last)
+						selectSuggestion(last);
+				}
 			}
-		} else if (e.which == 38) { // if up arrow
-			e.preventDefault();
-			var prev = (selected).prev('.suggestion');
-			if (prev.length != 0) {
-				selectSuggestion(prev);
-			} else {
-				var last = $('.suggestion').last();
-				if (last)
-					selectSuggestion(last);
-			}
-		} else if (e.which == 13) { // if enter
-			onUserChoice();
 		}
 	});
 	
@@ -191,7 +228,15 @@ $(document).ready(function(){
 		$(this).closest('.suggestion').addClass('selected');
 		onUserChoice();
 	});
+
+	// On keyup
+	$('#commandField').keyup(function(e) {
+		if (e.which == 13) { // If Enter key
+			onUserChoice();
+		}
+	});
 });
+
 
 /* ==============================================================================
  *     UTILITY FUNCTIONS
@@ -201,10 +246,10 @@ $(document).ready(function(){
 /*
  * Calls a function given the funciton's name
  */
-function callFunctionFromStr(functionName, params) {
-	params = params || {};
-	window[functionName](params);
+function callFunctionFromStr(functionName, args) {
+	window[functionName](args);
 }
+
 
 /*
  * Returns the believed Operating System of the user
